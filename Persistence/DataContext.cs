a8a8fe770cf559f4f82567cf155e23ce7367fef7
codyvogt <cody.vogt@caltechsurveys.com>
2019-09-03 15:11:05 -0600

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<WeatherForcast> WeatherForcasts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<WeatherForcast>()
+                .HasData(
+                    new WeatherForcast{
+                        Id = 1,
+                        Date = DateTime.Now,
+                        TemperatureC = 30,
+                        Summary = "It is warm out today"
+                    },
+                    new WeatherForcast{
+                        Id = 2,
+                        Date = DateTime.Now.AddDays(-1),
+                        TemperatureC = 20,
+                        Summary = "It is cold out today"
+                    },
+                    new WeatherForcast{
+                        Id = 3,
+                        Date = DateTime.Now.AddDays(-2),
+                        TemperatureC = 45,
+                        Summary = "It is HOT out today"
+                    }
+                );
+        }
+    }
+}
---
+++ Persistence/Migrations/20190903205833_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "WeatherForcasts",
+                columns: new[] { "Id", "Date", "Summary", "TemperatureC" },
+                values: new object[] { 1, new DateTime(2019, 9, 3, 14, 58, 33, 673, DateTimeKind.Local).AddTicks(4380), "It is warm out today", 30 });
+
+            migrationBuilder.InsertData(
+                table: "WeatherForcasts",
+                columns: new[] { "Id", "Date", "Summary", "TemperatureC" },
+                values: new object[] { 2, new DateTime(2019, 9, 2, 14, 58, 33, 675, DateTimeKind.Local).AddTicks(5041), "It is cold out today", 20 });
+
+            migrationBuilder.InsertData(
+                table: "WeatherForcasts",
+                columns: new[] { "Id", "Date", "Summary", "TemperatureC" },
+                values: new object[] { 3, new DateTime(2019, 9, 1, 14, 58, 33, 675, DateTimeKind.Local).AddTicks(5142), "It is HOT out today", 45 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WeatherForcasts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "WeatherForcasts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "WeatherForcasts",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
